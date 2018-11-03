fdf9a18419612f57e35f60cfae3f90db0ece2771
nicsachin <nicsachin@users.noreply.github.com>
2018-11-04 05:29:27 +0530

    Add files via upload

---
+++ finaldash-20180918T015406Z-001/finaldash/final/js/data.js
@@ -0,0 +1,399 @@
+var dataSet = [["1", "RLM05061958JECI021982", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "ROSHAN LAL MAHLA", "05-06-1958", "Male", "Engineering", "Exec.Engineer(Civil)-EECI", "EE Civil"],
+
+
+    [ "2", "KCT01071958JECI021982", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KISHAN CHANDRA TIWARI", "01-07-1958", "Male", "Engineering", "Asst.Engineer(Civil)-AECI", "AE Civil"],
+
+    [ "3", "KSI08101958JECI031980", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KRIPAL SINGH", "08-10-1958", "Male", "Engineering", "Asst.Engineer(Civil)-AECI", "AE Civil"],
+
+
+    [ "4", "KPA01081969JECI121993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KUMAR PAL", "01-08-1969", "Male", "Engineering", "Asst.Engineer(Civil)-AECI", "AE Civil"],
+    
+
+
+    [ "5", "PKA16041960JECI091983", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "PARMOD KUMAR AHUJA", "16-04-1960", "Male", "Engineering", "Asst.Engineer(Civil)-AECI", "AE Civil"],
+    
+
+
+    [ "6", "RSK24071968JECI121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAMDARASH SINGH KUSHWAHA", "24-07-1968", "Male", "Engineering", "Asst.Engineer(Civil)-AECI", "AE Civil"],
+    
+
+
+    [ "7", "RSU30051974LDCL081998", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAJIV SUNDRIYAL", "30-05-1974", "Male", "Accounts", "Asst.Accounts Officer-AAOF", "AAO"],
+    
+
+
+    [ "8", "JKK05111968DMC3121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JATINDER KUMAR KALRA", "05-11-1968", "Male", "Drawing Staff", "Senior Draughtsman(C)-SDCI", "Senior Draughtsman(C)"],
+    
+
+
+    [ "9", "AKS20011985JECI042013", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "ANISH KUMAR SHRIVASTAVA", "20-01-1985", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "10", "AKU09061980JECI092005", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "ASHOK KUMAR", "09-06-1980", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "11", "DKU15051970JECI052003", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "DAVENDER KUMAR", "15-05-1970", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "12", "NKU18111969LDCL061993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NARESH KUMAR", "18-11-1969", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "13", "NKS15121969DMC3111993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NARESH KUMAR SUHAG", "15-12-1969", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "14", "SKU05011964MATE081996", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SHARAWAN KUMAR", "05-01-1964", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "15", "VPA10101993JECI042013", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "VIVEK PATHAK", "10-10-1993", "Male", "Engineering", "Junior Engineer(Civil)-JECI", "JE Civil"],
+    
+
+
+    [ "16", "KCH04111966LDCL011989", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KHUSHAL CHAND", "04-11-1966", "Male", "Ministerial", "Upper Division Clerk-UDCL", "UDC"],
+    
+
+
+    [ "17", "KKK29041962LDCL091985", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KRISHAN KUMAR KALRA", "29-04-1962", "Male", "Ministerial", "Upper Division Clerk-UDCL", "UDC"],
+    
+
+
+    [ "18", "RSS31121958PEON011980", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RADHEY SHYAM SHARMA", "31-12-1958", "Male", "Ministerial", "Upper Division Clerk-UDCL", "UDC"],
+    
+
+
+    [ "19", "AKU25071992LDCL012015", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "AMIT KUMAR", "25-07-1992", "Male", "Ministerial", "Lower Division Clerk-LDCL", "LDC"],
+    
+
+
+    [ "20", "JMO24041967LDCL051993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JAYANTA MODAK", "24-04-1967", "Male", "Ministerial", "Lower Division Clerk-LDCL", "LDC"],
+    
+
+
+    [ "21", "PSI10061980LDCL102002", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "PRAHLAD SINGH", "10-06-1980", "Male", "Ministerial", "Lower Division Clerk-LDCL", "LDC"],
+    
+
+
+    [ "22", "RKU22101970LDCL011991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RISHI KUMAR", "22-10-1970", "Male", "Ministerial", "Lower Division Clerk-LDCL", "LDC"],
+    
+
+
+    [ "23", "ACH18111963CHOW041993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "AGAR CHAND", "18-11-1963", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "24", "HKU20081963PEON061990", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "HARISH KUMAR", "20-08-1963", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "25", "KSI21071964SWEE101986", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KAMAL SINGH", "21-07-1964", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "26", "RPR16071959CHOW041983", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAM PRASAD", "16-07-1959", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "27", "RPA15101961CHOW111991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAMAYAN PRASAD", "15-10-1961", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "28", "SSI16061962CHOW031987", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SARNAM SINGH", "16-06-1962", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "29", "SPR19021959CHOW051980", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SHIVNATH PRASAD", "19-02-1959", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "30", "SCH24041961CHOW051984", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SURESH CHAND", "24-04-1961", "Male", "Ministerial", "Multi Tasking Staff-MTST", "MTS"],
+    
+
+
+    [ "31", "RRP18021959CARP071988", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAJA RAM PRASAD", "18-02-1959", "Male", "Regular Classified", "Work Asstt-WOAS", "Work Asstt"],
+    
+
+
+    [ "32", "AKU09121960BELD021993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "ASHOK KUMAR", "09-12-1960", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "33", "BSI29111959BELD031987", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "BISHAN SINGH", "29-11-1959", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "34", "CHA01011960BELD051982", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "CHANDESHWAR", "01-01-1960", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "35", "CLA12091961BELD111988", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "CHHEDI LAL", "12-09-1961", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "36", "IPS02061964BELD061991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "INDER PAL SHARMA", "02-06-1964", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "37", "MAM23111964BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "MOHAMMAD AMIN", "23-11-1964", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "38", "NSI27061958BELD021993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NIRANJAN SINGH", "27-06-1958", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "39", "RPR04051960BELD041987", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAMANANDAN PRASAD", "04-05-1960", "Male", "Work Charged", "Carpenter-CARP", "Carpenter"],
+    
+
+
+    [ "40", "BRA02061964BELD051991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "BANARAS RAI", "02-06-1964", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "41", "GLM13081972BELD021997", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "GIANDI LAL MEENA", "13-08-1972", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "42", "GRP10061972BELD041992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "GIRI RAJ PRASAD", "10-06-1972", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "43", "JBH01051958BELD081988", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JAI BHAGWAN", "01-05-1958", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "44", "MSI16031958BELD041991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "MOHAR SINGH", "16-03-1958", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "45", "MUN03031964BELD081991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "MUNNI", "03-03-1964", "Female", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "46", "OPR18111958BELD011986", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "OM PRAKASH", "18-11-1958", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "47", "RKU15011960BELD111988", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAJ KUMAR", "15-01-1960", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "48", "SKU12111962BELD041991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SATISH KUMAR", "12-11-1962", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "49", "SPR05101961BELD051991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SAU PRASAD", "05-10-1961", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "50", "SBA10021959BELD021991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SHANKAR BAKSH", "10-02-1959", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "51", "VIS01051962MASO011993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "VISHNU", "01-05-1962", "Male", "Work Charged", "Mason-MASO", "Mason"],
+    
+
+
+    [ "52", "CSI11121963BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "CHANDAN SINGH", "11-12-1963", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "53", "JPM10081967BELD071992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JAGDISH PRASAD MEENA", "10-08-1967", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "54", "JUG16081962BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JUGESH", "16-08-1962", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "55", "MAH05061966PLUM021994", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "MAHESH", "05-06-1966", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "56", "NCH23061959PLUM121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NANAK CHAND", "23-06-1959", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "57", "NRA13041962BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NAND RAM", "13-04-1962", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "58", "PAR05041963BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "PARSHADI", "05-04-1963", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "59", "RAG05071963BELD051991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAGHU NATH SINGH", "05-07-1963", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+    [ "60", "RAM20051961BELD041991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAMVIR", "20-05-1961", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "61", "RLM07071971BELD101994", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RATAN LAL MEENA", "07-07-1971", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "62", "SLA01101958BELD051991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SONE LAL", "01-10-1958", "Male", "Work Charged", "Plumber-PLUM", "Plumber"],
+    
+
+
+    [ "63", "JPA15031959SEWM121983", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JAI PAL", "15-03-1959", "Male", "Regular Classified", "Sewerman-SEWM", "Sewerman"],
+    
+
+
+    [ "64", "MSI06031959SWEE121980", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "MAHENDER SINGH", "06-03-1959", "Male", "Regular Classified", "Sewerman-SEWM", "Sewerman"],
+    
+
+
+    [ "65", "AMI01031990BELD082011", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "AMIT", "01-03-1990", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "66", "APR18101967BELD121994", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "ATMA PRASAD", "18-10-1967", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "67", "BCM10011971BELD102014", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "BHAG CHAND MEENA", "10-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "68", "CLA01071958BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "CHAMAN LAL", "01-07-1958", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "69", "DSM01011971BELD122011", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "DEEWAN SINGH MEENA", "01-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "70", "DCM10011971BELD022015", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "DHARAM CHAND MEENA", "10-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "71", "DS415061964BELD051993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "DILIP SINGH", "15-06-1964", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "72", "DNM12041967BELD051993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "DINA NATH MAJHI", "12-04-1967", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+    [ "73", "GB414031965BELD041993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "GOPAL BAHADUR", "14-03-1965", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "74", "GLM03011971BELD112011", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "GOPAL LAL MEENA", "03-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "75", "GRA20081960BELD091993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "GOPAL RAM", "20-08-1960", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "76", "GSM08011971BELD102014", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "GUMAN SINGH MEENA", "08-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "77", "JRM02011971BELD122011", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "JATI RAM MEENA", "02-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "78", "KCM10011971BELD102014", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KAMAL CHAND MEENA", "10-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "79", "KLM05011971BELD012014", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KANCHAN LAL MEENA", "05-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "80", "KLA30031959BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KANJI LAL", "30-03-1959", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "81", "KAN01021963BELD011993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KANTASWAMY", "01-02-1963", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "82", "KAR20121961BELD041993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KARAPPAN", "20-12-1961", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "83", "KBA20021963BELD041993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "KRISHNA BAITHA", "20-02-1963", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "84", "MAH04101968BELD101994", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "MAHENDER SINGH", "04-10-1968", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "85", "NKU15031964BELD051992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NARESH KUMAR", "15-03-1964", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "86", "NKU31031966BELM091993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NARESH KUMAR", "31-03-1966", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "87", "NAW05121964BELD091993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "NAWABUDDIN", "05-12-1964", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "88", "OPR30061966BELD091993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "OM PRAKASH", "30-06-1966", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "89", "PCM02011971BELD122013", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "PRAKASH CHAND MEENA", "02-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "90", "PKM20081973BELD122005", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "PREM KUMAR MEENA", "20-08-1973", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+    
+
+
+    [ "91", "RKU04061963BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAJ KUMAR", "04-06-1963", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "92", "RVR03041962BELD061991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAM VRIKSHA", "03-04-1962", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "93", "RAM01011971BELD112011", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAM AVTAR MEENA", "01-01-1971", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+    [ "94", "RPA24021962BELD051993", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAM PARKASH", "24-02-1962", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "95", "RAM02041960BELD121992", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "RAMESH", "02-04-1960", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "96", "SKU15101976BELD122006", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SATISH KUMAR", "15-10-1976", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "97", "SKU30061964BELD121994", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SUBASH KUMAR", "30-06-1964", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "98", "SKU14061962BELD051991", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SURENDER KUMAR", "14-06-1962", "Male", "Work Charged", "Beldar-BELD", "Beldar"],
+
+
+    [ "99", "SKU01071960BELD122006", "DR-ADG(DR)-NDZ III-DELHI CIRCLE VI-S DIVN", "SURESH KUMAR", "01-07-1960", "Male", "Work Charged", "Beldar-BELD", "Beldar" ]];
+
+
+
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Sno." },
+            { title: "Employee Code" },
+            { title: "OfficeName" },
+            { title: "Employee Name" },
+            { title: "Date of birth" },
+            { title: "Gender" },
+            { title: "Staff Category" },
+            { title: "Designation" },
+            { title: "Post Held" }
+        ]
+    } );
+} );    
+  
